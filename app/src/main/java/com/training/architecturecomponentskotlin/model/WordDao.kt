4a4ce705bfb2af99ba5dd3f63616d3beadd44812
BHAVNA THACKER <bhavnathacker14@gmail.com>
2018-12-17 13:39:03 +0530

    Created WordDao

---
+++ app/src/main/java/com/training/architecturecomponentskotlin/model/WordDao.kt
@@ -0,0 +1,23 @@
+package com.training.architecturecomponentskotlin.model
+
+import android.arch.lifecycle.LiveData
+import android.arch.persistence.room.*
+
+@Dao
+interface WordDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertWord(word:Word)
+
+    @Delete
+    fun deleteWord(word:Word)
+
+    @Query("DELETE FROM word_table")
+    fun deleteAllWords()
+
+    @Query("SELECT * FROM word_table ORDER BY name ASC")
+    fun getAllWords(): LiveData<List<Word>>
+
+    @Query("SELECT * FROM word_table WHERE name = :name")
+    fun getWordByName(name:String): Word
+
+}
