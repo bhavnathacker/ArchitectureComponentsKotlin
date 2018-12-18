722e48fa9df537cc67bdd4b8addb216d778757b5
BHAVNA THACKER <bhavnathacker14@gmail.com>
2018-12-18 19:08:31 +0530

    Added Activity for New Word

---
+++ app/src/main/java/com/training/architecturecomponentskotlin/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.training.architecturecomponentskotlin.utils
+
+const val EXTRA_KEY_WORD = "key_word"
+const val EXTRA_KEY_MEANING = "key_meaning"
+const val RESULT_SAVE = 0
+const val RESULT_ERROR = 1
---
+++ app/src/main/java/com/training/architecturecomponentskotlin/view/NewWordActivity.kt
@@ -0,0 +1,49 @@
+package com.training.architecturecomponentskotlin.view
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import com.training.architecturecomponentskotlin.R
+import com.training.architecturecomponentskotlin.utils.EXTRA_KEY_MEANING
+import com.training.architecturecomponentskotlin.utils.EXTRA_KEY_WORD
+import com.training.architecturecomponentskotlin.utils.RESULT_ERROR
+import com.training.architecturecomponentskotlin.utils.RESULT_SAVE
+
+class NewWordActivity : AppCompatActivity() {
+
+    private lateinit var mWordEditText: EditText
+    private lateinit var mMeaningEditText: EditText
+    private lateinit var mSaveBtn: Button
+    private lateinit var mDeleteBtn: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_word)
+
+        mWordEditText = findViewById(R.id.word_text)
+        mMeaningEditText = findViewById(R.id.meaning_text)
+        mSaveBtn = findViewById(R.id.btn_save)
+        mDeleteBtn = findViewById(R.id.btn_delete)
+
+        mSaveBtn.setOnClickListener {
+            val intent = Intent()
+            if(TextUtils.isEmpty(mWordEditText.text) || TextUtils.isEmpty(mMeaningEditText.text)) {
+                setResult(RESULT_ERROR, intent)
+            } else {
+                intent.putExtra(EXTRA_KEY_WORD, mWordEditText.text.toString())
+                intent.putExtra(EXTRA_KEY_MEANING, mMeaningEditText.text.toString())
+                setResult(RESULT_SAVE, intent)
+            }
+            finish()
+
+        }
+
+        mDeleteBtn.setOnClickListener {
+            //TODO delete action
+        }
+    }
+}
