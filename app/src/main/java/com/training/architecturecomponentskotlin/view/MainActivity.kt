c6121d5c55fc8b3de52323f6f2e09d5eb50f5f87
BHAVNA THACKER <bhavnathacker14@gmail.com>
2018-12-18 16:38:59 +0530

    Added Recyclerview and Its Adapter

--- app/src/main/java/com/training/architecturecomponentskotlin/view/MainActivity.kt
+++ app/src/main/java/com/training/architecturecomponentskotlin/view/MainActivity.kt
@@ -3,18 +3,31 @@ package com.training.architecturecomponentskotlin.view
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
 import com.training.architecturecomponentskotlin.R
+import com.training.architecturecomponentskotlin.model.Word
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mRecyclerView:RecyclerView
+    private lateinit var mAdapter: WordListAdapter
+    private var mWords: List<Word> = mutableListOf<Word>(Word("Word Sample", "Word Example"), Word("Test", "Exam"))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        mRecyclerView = findViewById(R.id.recyclerView)
+        mAdapter = WordListAdapter(this)
+        mAdapter.setWords(mWords)
+        mRecyclerView.adapter = mAdapter
+        mRecyclerView.layoutManager = LinearLayoutManager(this)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
---
+++ app/src/main/java/com/training/architecturecomponentskotlin/view/WordListAdapter.kt
@@ -0,0 +1,40 @@
+package com.training.architecturecomponentskotlin.view
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.training.architecturecomponentskotlin.R
+import com.training.architecturecomponentskotlin.model.Word
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class WordListAdapter(private val mContext: Context) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>() {
+
+    private lateinit var mWords: List<Word>
+
+    fun getWords() = mWords
+
+    fun setWords(words: List<Word>){
+        mWords = words
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        return WordViewHolder(LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return mWords.size
+    }
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        var currentWord = mWords[position]
+        holder.wordTextView.text = currentWord.name
+    }
+
+    class WordViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var wordTextView: TextView = itemView.findViewById(R.id.textView)
+    }
+}
