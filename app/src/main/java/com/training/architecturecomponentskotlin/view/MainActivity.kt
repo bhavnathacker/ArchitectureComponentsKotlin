65df9f7fe42af139cd63153f1215e5560d7463a0
BHAVNA THACKER <bhavnathacker14@gmail.com>
2018-12-18 15:05:57 +0530

    Add XML Layout

--- app/src/main/java/com/training/architecturecomponentskotlin/view/MainActivity.kt
+++ app/src/main/java/com/training/architecturecomponentskotlin/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.training.architecturecomponentskotlin
+package com.training.architecturecomponentskotlin.view
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.training.architecturecomponentskotlin.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
